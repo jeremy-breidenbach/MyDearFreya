aa09ef0dd22e74cf629dce0557cc091890089225
Jeremy Breidenbach <jeremy.breidenbach@gmail.com>
2015-12-17 08:43:34 -0600

    Updated README and included Freya script

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # MyDearFreya
 Script to install Elementary OS 0.3 (Freya) onto your Chromebook
+
+Courtesy of Reddit user Skitals, more discussion here: http://www.reddit.com/r/Crouton/comments/32akho/any_guides_to_getting_the_new_elementary_freya/
+
+## How to Install
+1. Put the Chromebook in Developer mode by holding Esc + Refresh and tapping the Power button
+2. Download Crouton script  https://github.com/dnschneid/crouton
+3. Download MyDearFreya script, either at https://drive.google.com/folderview?id=0B5m4MQHPNKZjc1FfWldnWnZyM2M&usp=sharing, or from this GitHub repository
+4. Make sure freya.sh and crouton are in your ~/Downloads folder. To install, from a crosh shell run:
+```shell
+$ cd ~/Downloads
+$ sudo sh freya.sh
+```
+5. This script is not fully automated (yet)... follow the prompts, and enter the commands as directed.
+6. NOTE: After the OS is installed, you must return to the crosh shell and enter ```sudo sh freya.sh continue``` to get the startelementary script!
+7. After installation, you can enter elementary from a crosh shell with:
+```shell
+$ sudo startelementary
+```
+
+
+## After Install
+1. There may be updates pushed into elementary OS Freya since the last release. Install these updates for updated packages, security patches, and improved performance.
+
+    To do so, click on your Applications bar and launch Software Updates. It will automatically check for updates the first time you launch it. Click “Install Updates” to begin the update.
+
+2. Upon using Elementary OS you may find that your changes in System Settings do not save; if so, run the following command from terminal
+```shell
+$ sudo chown -R $USER:$USER /home/$USER/.config
+```
+3. You may also find that the Software Center does not work; if so, run the following command from terminal
+```shell
+$ sudo chown -R $USER:$USER /home/$USER/.cache
+```
+4. You can add the Elementary Tweaks tool to your System Settings to allow additional UI Tweaks
+```shell
+$ sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily
+$ sudo apt-get update
+$ sudo apt-get install elementary-tweaks
+```
+5. Install Ubuntu Restricted Extras to get codecs for playing mp3's
+    ```shell
+    $ sudo apt-get install ubuntu-restricted-extras
+    ```
+6. Here are a few links showing more things you can do after installing Elementary OS. These include installing alternative web browsers like Firefox or Chrome, or installing other programs:
+
+    * http://itsfoss.com/things-todo-elementary-os-freya/
+    * http://fuckyeah-elementaryos.tumblr.com/post/117983423013/things-to-do-after-installing-elementary-os-freya
+    * http://www.binarytides.com/better-elementary-os-luna/
+
+7. Anytime you need to switch between Chrome OS and Elementary OS, use CTRL+ALT+SHIFT+BACK or CTRL+ALT+SHIFT+FORWARD (the BACK and FORWARD keys are on the top row of your keyboard, next to Esc).
+
+
+## Changes
+### v0.04
+- fixed .config ownership which broke wallpaper and probably other stuff
+- disabled touchpad setting that was causing some users problems
+
+### v0.03
+- fixed Software Center (.cache permissions were wrong)
+- disabled network interface (Chrome OS controls this, it was causing Software Center to not work)
+- includes entire start script now instead of copying xfce script, which you didn't have if you've never installed the xfce4 target
+- reverted cursor size for non-Pixel users. Pixel users can uncomment the line in the script, or after booting, from terminal enter: gsettings set org.gnome.desktop.interface cursor-size 48
+
+### v0.02
+- No longer installs an initial DE target... works with only x11
+- Now adds sources via PPA instead of copying freya /etc/apt sources
+- Added touchpad and cursor tweaks
+- Fixed missing clock
+
+
+=======================================
+
+Based MyDearWatson by AldousP:
+https://github.com/AldousP/MyDearWatson
---
+++ freya.sh
@@ -0,0 +1,130 @@
+#!/bin/sh
+
+#MyDearFreya v0.04
+
+chroot () {
+	#Chroot specific functions that are run from within Trusty when this script is called with an argument of "installelementary"
+	#------------------------------------------------------------------
+    echo Setting language variables...
+    sudo sed -i '1s/^/LC_ALL=en_US.UTF-8\n/' /etc/environment
+    sudo sed -i '1s/^/LANGUAGE=en_US.UTF-8\n/' /etc/environment
+    sudo sed -i '1s/^/LANG=en_US.UTF-8\n/' /etc/environment
+    export LC_ALL=en_US.UTF-8
+    export LANG=en_US.UTF-8
+    export LANGUAGE=en_US.UTF-8
+    
+    echo Installing software-properties-common...
+    sudo apt-get -y -qq install software-properties-common
+    
+    echo Adding Elementary OS repositories...
+    sudo apt-add-repository -y ppa:elementary-os/os-patches
+    sudo apt-add-repository -y ppa:elementary-os/stable
+    
+    echo Updating package lists...
+    sudo apt-get -qq update
+    
+    echo Performing distribution upgrade...
+    sudo apt-get -y -qq dist-upgrade
+    
+    echo Installing elementary-desktop... this will take some time...
+	sudo apt-get -y -qq install elementary-desktop
+
+    echo Installing some UI tweaks...
+    #dbus-launch gsettings set org.gnome.settings-daemon.peripherals.touchpad disable-while-typing true
+    dbus-launch gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click true
+    dbus-launch gsettings set org.gnome.settings-daemon.peripherals.touchpad motion-acceleration 5.0
+    #dbus-launch gsettings set org.gnome.desktop.interface cursor-size 48
+    
+    echo Fixing permissions...
+    sudo chown -R $USER:$USER /home/$USER/.cache
+    sudo chown -R $USER:$USER /home/$USER/.config
+    sudo sed -i '1s/^/iface wlan0 inet manual\n/' /etc/network/interfaces
+    
+    echo Creating your startelementary scripts...
+	cd /usr/bin 
+	sudo echo '#!/bin/sh
+  exec xinit /usr/bin/xinit_pantheon' >> startelementary 
+	sudo chmod +x startelementary 
+	sudo chown root:root startelementary
+	sudo echo '#!/bin/sh
+  gnome-session --session=pantheon' >> xinit_pantheon 
+	sudo chmod +x xinit_pantheon 
+	sudo chown root:root xinit_pantheon
+    echo ==================================================
+    echo ==================================================
+    echo ==================================================
+    echo ==================================================
+    echo To continue...
+    echo Exit the chroot with the command:
+    echo '      exit'
+    echo ==================================================
+    echo ==================================================
+    echo if it hangs unmounting, hit Ctrl+C to return to the crosh shell...
+    echo and enter the following command from the chrosh shell:
+    echo '      sudo sh freya.sh continue'
+    echo ==================================================
+    echo ==================================================
+    echo ==================================================
+    echo ==================================================
+}
+
+chrome () {
+
+	sh -e crouton -r trusty -t x11,extension -n watson
+    echo ==================================================
+    echo ==================================================
+    echo ==================================================
+    echo ==================================================
+    echo To continue, enter your chroot:
+    echo '      sudo enter-chroot -n watson'
+    echo ==================================================
+    echo ==================================================
+    echo And from the chroot, enter the following command:
+    echo '      sudo sh ~/Downloads/freya.sh installelementary'
+    echo ==================================================
+    echo ==================================================
+    echo ==================================================
+    echo ==================================================
+}
+
+tryagain () {
+    C
+    echo Picking up where we left off...
+    echo =========================
+    scriptinstall
+}
+
+scriptinstall () {
+sudo echo '#!/bin/sh -e
+# Copyright (c) 2014 The crouton Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+
+APPLICATION="${0##*/}"
+
+USAGE="$APPLICATION [options]
+
+Wraps enter-chroot to start an Xfce session.
+By default, it will log into the primary user on the first chroot found.
+
+Options are directly passed to enter-chroot; run enter-chroot to list them."
+
+exec sh -e "`dirname "\`readlink -f "$0"\`"`/enter-chroot" -n watson "$@" "" \
+    exec startelementary' >> /usr/local/bin/startelementary
+    sudo chown root:root /usr/local/bin/startelementary
+    sudo chmod +x /usr/local/bin/startelementary
+    echo All done, entering Elementary OS Freya...
+    sleep 1
+	sudo startelementary
+
+}
+
+
+if [ "$1" = "installelementary" ]
+then chroot
+elif [ "$1" = "continue" ]
+then tryagain
+else chrome
+fi
